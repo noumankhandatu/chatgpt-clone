6de781aa5903b41be93a319564f643d5eedbb430
noumankhan <noumankhandatu>
2023-05-19 07:49:05 +0500

    text

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import {useState} from 'react';
-import axios from 'axios';
-import CGLogo from './chatGPT.png';
-import AppLogo from './app-logo.png';
-import './App.css';
+import { useState } from "react";
+import axios from "axios";
+import CGLogo from "./chatGPT.png";
+import AppLogo from "./app-logo.png";
+import "./App.css";
 
 function App() {
-  const [prompt, setPrompt] = useState('');
-  const [response, setResponse] = useState('');
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState("");
   const [loading, setLoading] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setLoading(true);
-    
+
     // communicate with API
-    // post input value 'prompt' to API end point 
+    // post input value 'prompt' to API end point
     axios
-      .post("http://localhost:5555/chat", { prompt })
+      .post("https://chatgpt-api-jcug.vercel.app/", { prompt })
       .then((res) => {
         setResponse(res.data);
         setLoading(false);
@@ -24,27 +24,19 @@ function App() {
       .catch((err) => {
         console.error(err);
       });
-    
   };
 
   return (
     <div className="wrapper">
-      <img src={AppLogo} alt="" className="app-logo" />	
+      <img src={AppLogo} alt="" className="app-logo" />
       <form onSubmit={handleSubmit}>
-        <img src={CGLogo} alt="" className={loading ? 'cg-logo loading' : 'cg-logo'} />
-        <input
-          type="text"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-          placeholder="Ask anything... :)"
-        />
+        <img src={CGLogo} alt="" className={loading ? "cg-logo loading" : "cg-logo"} />
+        <input type="text" value={prompt} onChange={(e) => setPrompt(e.target.value)} placeholder="Ask anything... :)" />
         <button type="submit">Ask</button>
       </form>
-      <p className="response-area">
-        {loading ? 'loading...' : response}
-      </p>
+      <p className="response-area">{loading ? "loading..." : response}</p>
       <div className="footer">~ webstylepress ~</div>
-</div>
+    </div>
   );
 }
 
